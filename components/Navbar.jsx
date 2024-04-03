8b6d573a68d5470261caa08ecf8d8a49225ee07f
Max de Vocht <maxdevocht@hotmail.com>
2024-04-03 15:02:06 +0200

    Update
    Added Navbar.jsx

---
+++ components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="nav--name">
+                <p className="name">Rijksmuseum</p>
+            </div>
+            <div className="nav--btn">
+                <button className="btn">Menu</button>
+            </div>
+        </nav>
+    )
+}
