b58e86a6420b3461cc7ecf5c7b566e50ea0c36df
Max de Vocht <maxdevocht@hotmail.com>
2024-04-04 14:22:06 +0200

    Update
    Added museum div

--- components/Bento_Header.jsx
+++ components/Bento_Header.jsx
@@ -3,6 +3,20 @@ import React from "react"
 export default function Bento_Header() {
     return (
         <header className="header">
+            
+            <div className="div--museum">
+                <div className="div--title">
+                    <h1 className="painting--name">RIJKSMUSEUM</h1>
+                </div>
+                <div className="museum--img"></div>
+                <p className="description">
+                    Ontdek de kunst en geschiedenis van Nederland in het Rijksmuseum. Bewonder de meesterwerken van Rembrandt, Vermeer en Van Gogh. Er is elke dag iets te beleven. Bestel nu je tickets en bezoek het Rijksmuseum!
+                </p>
+                <div className="div--link">
+                    <button className="link--btn">BESTEL TICKETS</button>
+                </div>
+            </div>
+
             <div className="div1">
                 <div className="div--title">
                     <h2 className="label">Nu nieuw te zien</h2>
