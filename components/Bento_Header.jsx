8a0cc480f0c2d24cda656c4d602877e6c4951cdb
Max de Vocht <maxdevocht@hotmail.com>
2024-04-04 13:12:08 +0200

    Update
    First div styling

---
+++ components/Bento_Header.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+export default function Bento_Header() {
+    return (
+        <header className="header">
+            <div className="div1">
+                <div className="div--title">
+                    <h2 className="label">Nu nieuw te zien</h2>
+                    <h1 className="painting--name">3X VAN GOGH</h1>
+                </div>
+                <div className="div--img"></div>
+                <div className="div--link">
+                    <div className="link">ONTDEK</div>
+                    <a href="#"><i class="ri-arrow-right-fill"></i></a>
+                </div>
+            </div>
+        </header>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Navbar from '../components/Navbar'
+import Bento_Header from '../components/Bento_Header'
 import './App.css'
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Bento_Header />
     </>
   )
 }
